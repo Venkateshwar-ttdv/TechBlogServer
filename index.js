a64eaa59cb9de3b8e705395bb3ac1d5fb52242b8
Venkateshwar-ttdv <142992105+Venkateshwar-ttdv@users.noreply.github.com>
2023-08-27 21:53:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/login" ,async(req ,res) => {
         if(passOk) {
             jwt.sign({username ,id : userDoc._id},secret ,{} ,(err ,token) => {
                 if(err) throw err;
-                   res.cookie('token' ,token,{sameSite : "none"}).json({
+                   res.cookie('token' ,token).json({
                    id : userDoc._id,
                    username
                 });
